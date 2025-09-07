dc7e3add52eae517a28a1a377e4f39d8cced567c
madi <mahdigtx00@gmail.com>
2025-09-07 23:40:10 +0200

    new functionalities

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -5,11 +5,11 @@ import { Dialog, DialogPanel } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
-  { name: 'Log in', href: '#' },
+  { name: 'Hem', href: '#' },
+  { name: 'Barbers', href: '#' },
+  { name: 'Prislista', href: '#' },
+  { name: 'Om oss', href: '#' },
+  { name: 'Kontakt', href: '#' },
 ]
 
 export default function Header() {
@@ -25,13 +25,15 @@ export default function Header() {
                 <span className="sr-only">Your Company</span>
                 <img
                   alt="Your Company"
-                  src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-                  className="h-8 w-auto dark:hidden"
+                  // src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+                  src="/gold-scissor.png"
+                  className="h-10 w-auto dark:hidden"
                 />
                 <img
                   alt="Your Company"
-                  src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
-                  className="h-8 w-auto not-dark:hidden"
+                  // src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+                  src="/gold-scissor.png"
+                  className="h-10 w-auto not-dark:hidden"
                 />
               </a>
               <button
@@ -134,17 +136,27 @@ export default function Header() {
                 <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8 dark:text-gray-400">
                   Slå dig ner i en av våra sköna barberarstolar, drick en god kopp kaffe medan våra erfarna barberare ser till att du blir snygg i både hår och skägg. Välkommen!
                 </p>
-                <div className="mt-10 flex items-center gap-x-6">
+                <div className="mt-10 flex items-center gap-x-14">
                   <a
                     href="https://koalendar.com/e/klippning"
-                    className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:hover:bg-indigo-400 dark:focus-visible:outline-indigo-500"
+                    className="rounded-md bg-indigo-600 px-8.5 py-5.5 text-xl  font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:hover:bg-indigo-400 dark:focus-visible:outline-indigo-500"
                   >
                     BOKA TID
                   </a>
-                  <a href="#" className="text-sm/6 font-semibold text-gray-900 dark:text-white">
+                  <a href="#" className="text-2xl font-semibold text-gray-900 dark:text-white">
                     SE PRISLITA
                     <span aria-hidden="true">→</span>
                   </a>
+                </div> 
+                <div className="mt-10 flex items-center gap-x-2.5">
+                  <div
+                    className="rounded-md px-3.5 py-3.5 text-lg border-2 font-semibold text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2  bg-zinc-900"
+                  >
+                     ⭐ 4.8 
+                  </div>
+                   <div className="text-lg font-semibold text-gray-900 dark:text-white">
+                    i betyg på Google
+                  </div>
                 </div>
               </div>
             </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,181 @@
+"use client"
+
 import Image from "next/image";
 import Header from "./components/header";
 import Footer from "./components/footer";
 
+import React, { useState } from "react";
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
+
 export default function Home() {
+
+  const images = [
+  "/1.jpg",
+  "/2.jpg",
+  "/3.jpg",
+  "/4.jpg",
+  "/5.jpg",
+];
+
+const [index, setIndex] = useState<number>(0);
+
+  const nextImage = () => {
+    setIndex((prev: number) => (prev + 1) % images.length);
+  };
+
+  const prevImage = () => {
+    setIndex((prev) => (prev - 1 + images.length) % images.length);
+  };
+
 
   return (
     <div>
-
         <Header/>
 
-        <iframe 
-          src="https://koalendar.com/e/klippning?embed=true" 
-          width="100%"
-          height="800"
-        />
+        <div id="kalender" className="dark:bg-gray-900">
+          <iframe 
+            src="https://koalendar.com/e/klippning?embed=true" 
+            width="100%"
+            height="800"
+          />  
+        </div>
 
-    <div style={{ justifyContent: "center", alignItems: "center", display: "flex", marginTop: "10px", marginBottom: "50px" }}>
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d162.85551429398376!2d14.151129355756387!3d57.73927196337394!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465a7241d37e8de5%3A0x877d9ea1bb55cf65!2sBankomat!5e1!3m2!1sen!2sse!4v1756759389340!5m2!1sen!2sse"
-        width="600" 
-        height="450"
-         style={{border:0}} 
-        allowFullScreen
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade">
-      </iframe>
-    </div>
+      {/* style={{ justifyContent: "center", alignItems: "center", display: "flex", marginTop: "20px", marginBottom: "20px" }}  */}
 
-      <Footer/>
+        <div id="map" className="flex justify-center items-center my-4 gap-x-20 ">
+
+            <div className="gap-8 flex flex-col" >
+              <div className="w-70 font-extrabold text-4xl text-gray-900 dark:text-black">
+                När detaljerna räknas
+              </div>
+              <div className="w-70 text-lg font-semibold text-gray-900 dark:text-black">
+                En barbershop där detaljerna avgör och resultatet känns helt rätt för dig. Oavsett om du vill ha en klassisk fade, en modern frisyr eller ett välvårdat skägg, ser vi till att du går härifrån med en stil som håller både i stolen och i vardagen.
+              </div>
+              <div className="mt-2">
+                <a
+                  href="#kalender"
+                  className="rounded-md bg-indigo-600 px-8.5 py-5.5 text-xl  font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:hover:bg-indigo-400 dark:focus-visible:outline-indigo-500"
+                >
+                  BOKA TID
+                </a>
+              </div>
+            </div>
+
+           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2282.166519508599!2d14.150451376968974!3d57.73934533726135!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465a7241dbc9dd0f%3A0xd1830ac9bc3949ae!2zRGlyZWt0ZW4gUsOlc2zDpHR0!5e1!3m2!1sen!2sse!4v1757242072758!5m2!1sen!2sse" width="600" height="450" style={{border:0}} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+
+        </div>
+
+
+        <div style={{justifyContent: "center" }} className="dark:bg-gray-900 flex items-center gap-x-50">
+
+          <div className="mt-10 mb-10 flex items-center gap-x-5">
+            <div
+              className="rounded-md px-3.5 py-3.5 text-lg border-2 font-semibold text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2  bg-zinc-50"
+            >
+
+                <a
+                  href="https://maps.app.goo.gl/HUzws8L1DhoqQFEh9"
+                >
+                  <img className="w-22 h-22" src="/gps-icon.png" alt="" />
+                </a> 
+
+            </div>
+
+            <div style={{ display: "flex", flexDirection: "column" }} className="gap-8" >
+              <div className="font-extrabold text-4xl text-gray-900 dark:text-white">
+                <a
+                  href="https://maps.app.goo.gl/HUzws8L1DhoqQFEh9"
+                >
+                  Besöksadress
+                </a>
+              </div>
+              <div className="text-lg font-semibold text-gray-900 dark:text-white">
+                <a
+                  href="https://maps.app.goo.gl/HUzws8L1DhoqQFEh9"
+                >
+                  Sparvhöksgatan 1, 556 11  
+                  <br />
+                  Jönköping       
+                </a>                
+              </div>
+            </div>
+
+          </div>
+
+          <div className="mt-10 mb-10 flex items-center gap-x-5">
+            <div
+              className="rounded-md px-3.5 py-3.5 text-lg border-2 font-semibold text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2  bg-zinc-50"
+            >
+              <a
+                href="tel:+46761882188"
+              >
+                <img className="w-22 h-22" src="/phone-icon.png" alt="" />
+              </a>
+
+            </div>
+
+            <div style={{ display: "flex", flexDirection: "column" }} className="gap-8" >
+              <div className="font-extrabold text-4xl text-gray-900 dark:text-white">
+                <a
+                  href="tel:+46761882188"
+                >
+                  +46761882188
+                </a>
+                
+              </div>
+              <div className="text-lg font-semibold text-gray-900 dark:text-white">
+                Måndag - Lördag: 10:00 - 19:00
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+
+        {/* <div style={{backgroundColor: "white", margin: "12px"}}></div> */}
+
+        <div className="flex justify-center items-center p-8 my-4 gap-x-13">
+        {/* Text + Arrows on the right */}
+          <div className="flex flex-col">
+
+            <div className="gap-8 flex flex-col" >
+              <div className="font-extrabold text-6xl text-gray-900 dark:text-black">
+                Galleri
+              </div>
+              <div className="w-70 text-lg font-semibold text-gray-900 dark:text-black">
+              Ta en titt på våra senaste klippningar, noggrant utförda av våra skickliga barberare i Stockholm. Våra barberare hjälper gärna dig att hitta en look som känns rätt för dig.           
+              </div>
+            </div>
+
+            {/* Arrows */}
+            <div className="flex gap-6 mt-10">
+              <button
+                onClick={prevImage}
+                className="p-3 rounded-full bg-gray-200 hover:bg-gray-300 transition"
+              >
+                <HiChevronLeft size={24} />
+              </button>
+              <button
+                onClick={nextImage}
+                className="p-3 rounded-full bg-gray-200 hover:bg-gray-300 transition"
+              >
+                <HiChevronRight size={24} />
+              </button>
+            </div>
+
+          </div>
+
+          {/* Image on the left */}
+          <div className="">
+            <img
+              src={images[index]}
+              alt={`Gallery image ${index + 1}`}
+              className="w-2xl h-[800px] object-cover rounded-xl shadow-md"
+          />
+          </div>
+        </div>        
+
+        <Footer/>
 
         {/* <h1 className="text-3xl font-bold underline text-center">   calendly    </h1>
 
@@ -56,7 +204,7 @@ export default function Home() {
             height="100%"
           />
         </div> */}
-  
+
     </div>
   );
 }
